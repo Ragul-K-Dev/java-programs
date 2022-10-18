f03557c0e4283df01326afed7a9366dd8a04b513
ragul <ragul.k1334@gmail.com>
2022-10-18 20:59:04 +0530

    cd

---
+++ Ragul.java
@@ -0,0 +1,14 @@
+class Ragul{
+
+  String parentName ="Ragul.k";
+  int  acountNo = 23456;
+
+void playing(){
+  System.out.println("playing Cricket");
+}
+
+private void acountNo(){
+ acountNo=23456;
+  System.out.println("acountNo");
+}
+}
